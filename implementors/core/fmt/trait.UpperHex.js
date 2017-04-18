589426741af9cd97cde3e8f2e038a6c86a0ed614
Jan S <jan.schreib@gmail.com>
2017-04-18 23:24:49 +0200

    updated doc

--- implementors/core/fmt/trait.UpperHex.js
+++ implementors/core/fmt/trait.UpperHex.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["daemonize"] = [];
 implementors["libc"] = [];
 
             if (window.register_implementors) {
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["daemonize"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='daemonize/enum.DaemonizeError.html' title='daemonize::DaemonizeError'>DaemonizeError</a>",];
+implementors["libc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
